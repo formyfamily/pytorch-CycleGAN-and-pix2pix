c6517c738d9e86bd3b0f7996ac44e9fbe8c0d2eb
Jiaman Li <jli@smtp.ict.usc.edu>
2020-01-09 22:53:22 -0800

    Sync

---
+++ data/facetextest_dataset.py
@@ -0,0 +1,181 @@
+import os.path
+from data.base_dataset import BaseDataset, get_transform
+from data.image_folder import make_dataset
+from PIL import Image
+import imageio
+import random
+import torch
+import numpy as np
+import json
+
+def load_img(filename):
+    _format = 'EXR-FI' if filename.split(".")[-1] == "exr" else None
+    return imageio.imread(filename, format=_format)
+
+def scale_to_range(exp):
+    x_min = -10.7578125
+    x_max = 11.5546875
+    y_min = -22.625
+    y_max = 14.578125
+    z_min = -6.078125
+    z_max = 14.125
+
+    # Normalize to 0~1
+    normalized_exp_x = (exp[:, :, 0].unsqueeze(-1)-x_min)/(x_max-x_min) # H X W X 1
+    normalized_exp_y = (exp[:, :, 1].unsqueeze(-1)-y_min)/(y_max-y_min)
+    normalized_exp_z = (exp[:, :, 2].unsqueeze(-1)-z_min)/(z_max-z_min)
+
+    normalized_exp = torch.cat((normalized_exp_x, normalized_exp_y, normalized_exp_z), dim=-1) # H X W X 3
+
+    scaled_exp = normalized_exp*2-1 # Scale to -1 ~1
+
+    return scaled_exp
+
+class FacetexTestDataset(BaseDataset):
+    """
+    This dataset class can load unaligned/unpaired datasets.
+
+    It requires two directories to host training images from domain A '/path/to/data/trainA'
+    and from domain B '/path/to/data/trainB' respectively.
+    You can train the model with the dataset flag '--dataroot /path/to/data'.
+    Similarly, you need to prepare two directories:
+    '/path/to/data/testA' and '/path/to/data/testB' during test time.
+    """
+
+    def __init__(self, opt):
+        """Initialize this dataset class.
+
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseDataset.__init__(self, opt)
+
+        self.ids_dic = json.load(open(opt.jsonfile, 'r'))
+
+        self.ids = []
+        for k in self.ids_dic.keys():
+            self.ids.append(int(k))
+
+        # self.ls_geo_folder = "/home/ICT2000/jli/local/data/LightStageFaceDB/256/PointCloud_Aligned"
+        # self.tr_geo_folder = "/home/ICT2000/jli/local/data/InfiniteRealities_Triplegangers/256/PointCloud_Aligned"
+
+        # self.ls_tex_folder = "/home/ICT2000/jli/local/data/LightStageFaceDB/256/DiffuseAlbedo"
+        # self.tr_tex_folder = "/home/ICT2000/jli/local/data/InfiniteRealities_Triplegangers/256/DiffuseAlbedo"
+        self.ls_geo_folder = "/home/ICT2000/jli/local/data/LightStageFaceDB/128/PointCloud_Aligned"
+        self.tr_geo_folder = "/home/ICT2000/jli/local/data/InfiniteRealities_Triplegangers/128/PointCloud_Aligned"
+
+        self.ls_tex_folder = "/home/ICT2000/jli/local/data/LightStageFaceDB/128/DiffuseAlbedo"
+        self.tr_tex_folder = "/home/ICT2000/jli/local/data/InfiniteRealities_Triplegangers/128/DiffuseAlbedo"
+
+        self.mask = torch.FloatTensor(np.load("mask_128.npy"))[:,:,None]
+
+    def load_blendshapes(self):
+        blendshape_folder = "/home/ICT2000/jli/local/data/Blendshapes_128_exr" 
+        bs_mask_folder = "/home/ICT2000/jli/mnt/glab2/jiaman/github/face_auto_rigging/codes/bs_mask_npy_128"
+        bs_order_txt = "/home/ICT2000/jli/mnt/glab2/Users/yajie/Siggraph2020_AutoRigging/BlendshapeOrder_12_6.txt"
+        exr_files = os.listdir(blendshape_folder)
+        
+        # Load neutral face
+        neutral_f = os.path.join(blendshape_folder, "Neutral_pointcloud.exr")
+        neutral_bs = torch.from_numpy(load_img(neutral_f)).float() # H X W X C
+
+        f = open(bs_order_txt, 'r')
+        lines = f.readlines()
+        
+        bs_img_list = []
+        mask_list = []
+        for ori_line in lines:
+            obj_name = ori_line.split()[0]
+            f_tag = obj_name.replace('.obj', '')
+
+            f_path = os.path.join(blendshape_folder, f_tag+"_pointcloud.exr")
+            bs_img = torch.from_numpy(load_img(f_path)).float() # H X W X C
+            bs_img_list.append(bs_img)
+
+            mask_path = os.path.join(bs_mask_folder, f_tag+"_pointcloud.npy")
+            mask_data = torch.from_numpy(np.load(mask_path)).float() # H X W
+            mask_list.append(mask_data)
+
+        bs_img_list = torch.stack(bs_img_list) # 55 X H X W X C
+        mask_list = torch.stack(mask_list) # 55 X H X W
+
+        scaled_bs_img_list = batch_scale_to_range(bs_img_list) # 55 X H X W X C
+        scaled_template_neutral = batch_scale_to_range(neutral_bs[None, :, :, :]) # 1 X H X W X C
+        bs_offsets = scaled_bs_img_list - scaled_template_neutral
+        masked_bs_offsets = mask_list[:, :, :, None] * bs_offsets # 55 X H X W X C
+
+        return masked_bs_offsets, mask_list
+        # 55 X H X W X C, 55 X H X W
+
+
+    def load_single_subject(self, index, exp_idx, load_geo=True):
+        source = self.ids_dic[index]['source']
+        f_tag_list = self.ids_dic[index]['f_tag'] # Already sorted
+
+        if load_geo:
+            postfix_str = "_pointcloud.exr"
+        else:
+            postfix_str = "_diffuse_albedo.exr"
+
+        if source == "ls":
+            if load_geo:
+                expression_folder = self.ls_geo_folder
+            else:
+                expression_folder = self.ls_tex_folder
+        elif source == "tr":
+            if load_geo:
+                expression_folder = self.tr_geo_folder
+            else:
+                expression_folder = self.tr_tex_folder
+
+        f_tag = f_tag_list[exp_idx]
+        expression_f_name = f_tag + postfix_str
+        expression_f_path = os.path.join(expression_folder, expression_f_name)
+        exp_img = torch.from_numpy(load_img(expression_f_path)).float() # H X W X C
+        
+        if load_geo:
+            scaled_exp = scale_to_range(exp_img)
+            masked_scaled_img = self.mask*scaled_exp
+        else:
+            masked_scaled_img = exp_img
+
+        return masked_scaled_img.transpose(0, 2).transpose(1, 2) # C X H X W
+
+    def load_neutral_face(self, index, load_geo=True):
+        if self.ids_dic[index]['source'] == "template":
+            neutral_face = self.load_single_subject(index, 0, load_geo)
+        elif self.ids_dic[index]['source'] == "ls":
+            neutral_face = self.load_single_subject(index, 1, load_geo)
+        elif self.ids_dic[index]['source'] == "tr":
+            neutral_face = self.load_single_subject(index, 0, load_geo)
+
+        return neutral_face 
+        
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index (int)      -- a random integer for data indexing
+
+        Returns a dictionary that contains A, B, A_paths and B_paths
+            A (tensor)       -- an image in the input domain
+            B (tensor)       -- its corresponding image in the target domain
+            A_paths (str)    -- image paths
+            B_paths (str)    -- image paths
+        """
+        index %= len(self.ids)
+        if index == 0:
+            return self.__getitem__(index+1) # Skip the template since template does not have corresponding textures
+        p_a_idx = str(index)
+
+        A_neutral_tex = self.load_neutral_face(p_a_idx, load_geo=False)
+        
+        num_expressions = len(self.ids_dic[p_a_idx]['f_tag'])
+        exp_idx = np.random.randint(num_expressions) # Index for single expression
+        A_geo = self.load_single_subject(p_a_idx, exp_idx, load_geo=True) # C X H X W
+        A_tex = self.load_single_subject(p_a_idx, exp_idx, load_geo=False)
+           
+        return {'A': torch.cat((A_neutral_tex, A_geo), dim=0), 'B': A_tex}
+
+    def __len__(self):
+        return len(self.ids)*20
---
+++ test_texture.py
@@ -0,0 +1,152 @@
+"""General-purpose test script for image-to-image translation.
+
+Once you have trained your model with train.py, you can use this script to test the model.
+It will load a saved model from --checkpoints_dir and save the results to --results_dir.
+
+It first creates model and dataset given the option. It will hard-code some parameters.
+It then runs inference for --num_test images and save results to an HTML file.
+
+Example (You need to train models first or download pre-trained models from our website):
+    Test a CycleGAN model (both sides):
+        python test.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
+
+    Test a CycleGAN model (one side only):
+        python test.py --dataroot datasets/horse2zebra/testA --name horse2zebra_pretrained --model test --no_dropout
+
+    The option '--model test' is used for generating CycleGAN results only for one side.
+    This option will automatically set '--dataset_mode single', which only loads the images from one set.
+    On the contrary, using '--model cycle_gan' requires loading and generating results in both directions,
+    which is sometimes unnecessary. The results will be saved at ./results/.
+    Use '--results_dir <directory_path_to_save_result>' to specify the results directory.
+
+    Test a pix2pix model:
+        python test.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
+
+See options/base_options.py and options/test_options.py for more test options.
+See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
+See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
+"""
+import os
+from options.test_options import TestOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import save_images
+from util import html
+import torch
+import torch.nn as nn
+import numpy as np
+from face.Face import FaceVis, FaceModel
+
+def recover_ori(exp):
+    # exp: H X W X C
+    exp = (exp+1)/2 # 0~1
+
+    x_min = -10.7578125
+    x_max = 11.5546875
+    y_min = -22.625
+    y_max = 14.578125
+    z_min = -6.078125
+    z_max = 14.125
+
+    ori_exp_x = exp[:, :, 0].unsqueeze(-1)*(x_max-x_min)+x_min # H X W X 1
+    ori_exp_y = exp[:, :, 1].unsqueeze(-1)*(y_max-y_min)+y_min 
+    ori_exp_z = exp[:, :, 2].unsqueeze(-1)*(z_max-z_min)+z_min
+
+    ori_exp = torch.cat((ori_exp_x, ori_exp_y, ori_exp_z), dim=-1) # H X W X 3
+
+    return ori_exp # H X W X 3
+
+def vis_geometry(pc_tensor, out_obj_path):
+    # load template
+    face_model = FaceModel()
+
+    # convert pc to facial mesh
+    face_model.update_pc(pc_tensor)
+
+    # save to output
+    face_model.export_to_obj(out_obj_path)
+
+if __name__ == '__main__':
+    opt = TestOptions().parse()  # get test options
+    # hard-code some parameters for test
+    opt.num_threads = 0   # test code only supports num_threads = 1
+    opt.batch_size = 1    # test code only supports batch_size = 1
+    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
+    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
+    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    
+    opt.dataset_mode = "facex"
+    paired_dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    opt.dataset_mode = "facebs"
+    bs_dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    
+    model = create_model(opt)      # create a model given opt.model and other options
+    model.setup(opt)               # regular setup: load and print networks; create schedulers
+    # create a website
+    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
+    if opt.load_iter > 0:  # load_iter is 0 by default
+        web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
+    print('creating web directory', web_dir)
+    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
+    # test with eval mode. This only affects layers like batchnorm and dropout.
+    # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
+    # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
+    if opt.eval:
+        model.eval()
+    pair_flag = False
+    total_iters = 0
+    # for i, data in enumerate(dataset):
+    for i, (paired_data, bs_data) in enumerate(zip(paired_dataset, bs_dataset)):
+        if i >= opt.num_test:  # only apply our model to opt.num_test images.
+            break
+        if pair_flag:
+            model.set_input(paired_data, pair_flag)  # unpack data from data loader
+        else:
+            model.set_input(bs_data, pair_flag)
+        model.test()           # run inference
+        # visuals = model.get_current_visuals()  # get image results
+        # img_path = model.get_image_paths()     # get image paths
+        # if i % 5 == 0:  # save images to an HTML file
+        #     print('processing (%04d)-th image... %s' % (i, img_path))                
+    
+        if pair_flag:
+            if not os.path.exists(os.path.join(opt.objpath, "paired_train", str(total_iters))):
+                os.makedirs(os.path.join(opt.objpath, "paired_train", str(total_iters)))
+        else:
+            if not os.path.exists(os.path.join(opt.objpath, "bs_train", str(total_iters))):
+                os.makedirs(os.path.join(opt.objpath, "bs_train", str(total_iters)))
+        for name, image in model.get_current_visuals().items():
+            if "offset" not in name:
+                if pair_flag: # Paired data training
+                    if "composed" not in name: # composed_B is only useful in bs training
+                        save_obj_path = os.path.join(opt.objpath, "paired_train", str(total_iters), "%d_%s.obj"%(total_iters, name))
+                        pc_tensor = recover_ori(image[0].transpose(0, 2).transpose(0, 1).detach().cpu()).transpose(0, 2).transpose(1, 2)
+                        vis_geometry(pc_tensor, save_obj_path)
+                else:
+                    if image.size()[1] == 55:
+                        if "neutral" not in name:
+                            if "real_A_input" not in name:
+                                for bs_idx in range(image.size()[1]):
+                                    if not os.path.exists(os.path.join(opt.objpath, "bs_train", str(total_iters), "%d_%s"%(total_iters, name))):
+                                        os.makedirs(os.path.join(opt.objpath, "bs_train", str(total_iters), "%d_%s"%(total_iters, name)))
+                                    save_obj_path = os.path.join(opt.objpath, "bs_train", str(total_iters), "%d_%s"%(total_iters, name), str(bs_idx)+".obj")
+                                    pc_tensor = recover_ori(image[0, bs_idx].transpose(0, 2).transpose(0, 1).detach().cpu()).transpose(0, 2).transpose(1, 2)
+                                    vis_geometry(pc_tensor, save_obj_path)
+                            else: # Template blendshape only generate once
+                                if not os.path.exists(os.path.join(opt.objpath, "bs_train", "%s"%(name))):
+                                    os.makedirs(os.path.join(opt.objpath, "bs_train", "%s"%(name)))
+                                    for bs_idx in range(image.size()[1]):
+                                        save_obj_path = os.path.join(opt.objpath, "bs_train", "%s"%(name), str(bs_idx)+".obj")
+                                        pc_tensor = recover_ori(image[0, bs_idx].transpose(0, 2).transpose(0, 1).detach().cpu()).transpose(0, 2).transpose(1, 2)
+                                        vis_geometry(pc_tensor, save_obj_path)
+                        else:
+                            save_obj_path = os.path.join(opt.objpath, "bs_train", str(total_iters), "%d_%s.obj"%(total_iters, name))
+                            pc_tensor = recover_ori(image[0, 0].transpose(0, 2).transpose(0, 1).detach().cpu()).transpose(0, 2).transpose(1, 2)
+                            vis_geometry(pc_tensor, save_obj_path)
+                    else:
+                        save_obj_path = os.path.join(opt.objpath, "bs_train", str(total_iters), "%d_%s.obj"%(total_iters, name))
+                        pc_tensor = recover_ori(image[0].transpose(0, 2).transpose(0, 1).detach().cpu()).transpose(0, 2).transpose(1, 2)
+                        vis_geometry(pc_tensor, save_obj_path)
+        total_iters += 1
+        # save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+    webpage.save()  # save the HTML
