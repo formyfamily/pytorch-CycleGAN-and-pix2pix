1b29a9f59fc321cd1c074cfe61247a5085d9da6d
Jiaman Li <jli@smtp.ict.usc.edu>
2020-01-09 22:53:47 -0800

    Sync

--- models/pix2pixtex_model.py
+++ models/pix2pixtex_model.py
@@ -30,7 +30,7 @@ class Pix2PixTexModel(BaseModel):
         """
         # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
         # parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
-        parser.set_defaults(norm='instance', netG='unet_256', dataset_mode='facetex')
+        parser.set_defaults(norm='instance', netG='unet_128', dataset_mode='facetex')
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
